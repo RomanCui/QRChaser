70856c8e74425294563d0419ff8732abfe4281c7
Matthew Davies <mfdavies@ualberta.ca>
2022-03-12 16:02:39 -0700

    Revert "Merge branch 'Matthew'"
    This reverts commit 5e391ce4478e57d64f005c5300388beced3435a8, reversing
    changes made to e3a8048b6d6411898f24c031b40cb58ce5b872a3.

--- code/app/src/main/java/com/example/qrchaser/QRCode.java
+++ code/app/src/main/java/com/example/qrchaser/QRCode.java
@@ -6,40 +6,12 @@ public class QRCode {
     private int hash;
     private String name;
     private int score;
-    private String id;
     // I am unsure as to how these two will be implmented
     //private location;
     //private image image;
     private ArrayList<QRComment> allComments = new ArrayList<>();
 
     // Unsure as to how the rest of the implementation will go
-    // Due to all of the requirements for this class, we should implement a
-    // second class for the player login qr code, if one is even needed (Could just make it on the fly)
+    // Due to all of the requirements for this class, we should implement a second class for the player login qr code
 
-    public QRCode(int hash, String name) {
-        this.hash = hash;
-        this.name = name;
-        //this.score = .... calculated from the hash
-        this.id =  name + "(" + hash + ")"; // Used for the Geolocation for sure
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getHash() {
-        return hash;
-    }
-
-    public int getScore() {
-        return score;
-    }
-
-    public String getId() {
-        return id;
-    }
 }// end Class
