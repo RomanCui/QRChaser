e87c7cc9fd0737f42018fe318cd4d6a4b8d84749
“Ronggang <ronggang999@gmail.com>
2022-03-13 17:39:35 -0600

    outline of format check

--- code/app/src/main/java/com/example/qrchaser/CreateAccountActivity.java
+++ code/app/src/main/java/com/example/qrchaser/CreateAccountActivity.java
@@ -12,6 +12,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
@@ -56,52 +57,70 @@ public class CreateAccountActivity extends AppCompatActivity {
                 String nickname = nicknameET.getText().toString();
                 String phone = phoneET.getText().toString();
 
-                // Access a Cloud Firestore instance from your Activity
-                db = FirebaseFirestore.getInstance();
+                int formatResult = formatCheck(emailAddress, password, phone);
 
-                // Get a top level reference to the collection
-                final CollectionReference collectionReference =
-                        db.collection("Accounts");
+                if( formatResult == 0){
+                    // Access a Cloud Firestore instance from your Activity
+                    db = FirebaseFirestore.getInstance();
 
-                HashMap<String, String> accounts = new HashMap<>();
-                if (emailAddress.length()>0 && password.length()>0 &&
-                        nickname.length()>0 && phone.length()>0) {
-                    // If there’s some data in the EditText field, then we create a new key-value pair.
-                    accounts.put("EmailAddress", emailAddress);
-                    accounts.put("Password", password);
-                    accounts.put("Nickname", nickname);
-                    accounts.put("Phone", phone);
-                }
+                    // Get a top level reference to the collection
+                    final CollectionReference collectionReference =
+                            db.collection("Accounts");
 
-                collectionReference
-                        .document(emailAddress)
-                        .set(accounts)
-                        .addOnSuccessListener(new OnSuccessListener<Void>() {
-                            @Override
-                            public void onSuccess(Void aVoid) {
-                            // These are a method which gets executed when the task is succeeded
-                                Log.d(TAG, "Data has been added successfully!");
-                            }
-                        })
-                        .addOnFailureListener(new OnFailureListener() {
-                            @Override
-                            public void onFailure(@NonNull Exception e) {
-                            // These are a method which gets executed if there’s any problem
-                                Log.d(TAG, "Data could not be added!" + e.toString());
-                            }
-                        });
-                emailET.setText("");
-                passwordET.setText("");
-                nicknameET.setText("");
-                phoneET.setText("");
-
-                collectionReference.addSnapshotListener(new EventListener<QuerySnapshot>() {
-                    @Override
-                    public void onEvent(@Nullable QuerySnapshot queryDocumentSnapshots, @Nullable
-                            FirebaseFirestoreException error) {
+                    HashMap<String, String> accounts = new HashMap<>();
+                    if (emailAddress.length()>0 && password.length()>0 &&
+                            nickname.length()>0 && phone.length()>0) {
+                        // If there’s some data in the EditText field, then we create a new key-value pair.
+                        accounts.put("EmailAddress", emailAddress);
+                        accounts.put("Password", password);
+                        accounts.put("Nickname", nickname);
+                        accounts.put("Phone", phone);
                     }
-                });
 
+                    collectionReference
+                            .document(emailAddress)
+                            .set(accounts)
+                            .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                @Override
+                                public void onSuccess(Void aVoid) {
+                                    // These are a method which gets executed when the task is succeeded
+                                    Log.d(TAG, "Data has been added successfully!");
+                                }
+                            })
+                            .addOnFailureListener(new OnFailureListener() {
+                                @Override
+                                public void onFailure(@NonNull Exception e) {
+                                    // These are a method which gets executed if there’s any problem
+                                    Log.d(TAG, "Data could not be added!" + e.toString());
+                                }
+                            });
+                    emailET.setText("");
+                    passwordET.setText("");
+                    nicknameET.setText("");
+                    phoneET.setText("");
+
+                    collectionReference.addSnapshotListener(new EventListener<QuerySnapshot>() {
+                        @Override
+                        public void onEvent(@Nullable QuerySnapshot queryDocumentSnapshots, @Nullable
+                                FirebaseFirestoreException error) {
+                        }
+                    });
+                }
+                else if(formatResult == 1){
+                    Toast.makeText(getApplicationContext(),
+                            "FAIL: Please check your email format",
+                            Toast.LENGTH_LONG).show();
+                }
+                else if(formatResult == 2){
+                    Toast.makeText(getApplicationContext(),
+                            "FAIL: Please check your password format",
+                            Toast.LENGTH_LONG).show();
+                }
+                else if(formatResult == 3){
+                    Toast.makeText(getApplicationContext(),
+                            "FAIL: Please check your phone number format",
+                            Toast.LENGTH_LONG).show();
+                }
             }
         });
 
@@ -111,5 +130,9 @@ public class CreateAccountActivity extends AppCompatActivity {
                 finish();
             }
         });
+    }
+
+    public int formatCheck(String email, String password, String phone){
+        return 0;
     }
 }
